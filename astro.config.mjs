5a9d859be4d1cfb9c3928969883c5f37722c233a
Felix Feuchter <felixzitzelsberger@yahoo.com>
2025-03-12 14:19:43 +0100

    added base to Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    outDir: './docs'
+    outDir: './docs',
+    output: 'static',
+    base: '/matuschek/'
 });
