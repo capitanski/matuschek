ca05a889707e4c86ac3d5830f203730c75c0c8a4
Felix Feuchter <felixzitzelsberger@yahoo.com>
2025-03-12 16:14:38 +0100

    built Mobile nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+// Make sure to uncomment the base when publishing to GITHUB PAGES
 // https://astro.build/config
 export default defineConfig({
     site: 'https://capitanski.github.io/',
